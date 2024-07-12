8a1223028634000aeaa38d5117e1cf45affc1835
bonedaddy <alex@bonedaddy.io>
2024-07-12 10:28:29 -0700

    idl_indexer: use base64 encoding

--- crates/sb_dl/src/idl.rs
+++ crates/sb_dl/src/idl.rs
@@ -1,5 +1,8 @@
+use std::time::Duration;
+
 use anyhow::Context;
-use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::RpcProgramAccountsConfig};
+use solana_account_decoder::UiAccountEncoding;
+use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig}};
 use solana_sdk::pubkey::Pubkey;
 
 const IDL_SEED: &str = "anchor:idl";
@@ -12,14 +15,20 @@ impl IdlIndexer {
     pub async fn new(
         endpoint: &str
     ) -> anyhow::Result<Self> {
-        let rpc = RpcClient::new(endpoint.to_string());
+        let rpc = RpcClient::new_with_timeout(endpoint.to_string(), Duration::from_secs(600));
         Ok(Self {rpc})
     }
     /// returns all possible accounts used to store idl's for deployed programs
     pub async fn get_idl_accounts(&self) -> anyhow::Result<()> {
         let program_accounts = self.rpc.get_program_accounts_with_config(
             &"BPFLoaderUpgradeab1e11111111111111111111111".parse::<Pubkey>()?,
-            RpcProgramAccountsConfig::default()
+            RpcProgramAccountsConfig {
+                account_config: RpcAccountInfoConfig {
+                    encoding: Some(UiAccountEncoding::Base64),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }
         ).await.with_context(|| "failed to get program accounts")?;
         let program_idls = program_accounts.into_iter().map(|(program, _)| {
 
