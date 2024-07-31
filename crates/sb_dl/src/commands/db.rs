998d07317c9d174dbe1d5ca877bad492178b2e0e
bonedaddy <alex@bonedaddy.io>
2024-07-31 12:05:21 -0700

    log blog info when repairing missing slot information

--- crates/sb_dl/src/commands/db.rs
+++ crates/sb_dl/src/commands/db.rs
@@ -1,12 +1,9 @@
 use clap::ArgMatches;
 use db::migrations::run_migrations;
 use sb_dl::config::Config;
-use solana_transaction_status::UiConfirmedBlock;
+use solana_transaction_status::{EncodedTransaction, UiConfirmedBlock};
 
-pub async fn fill_missing_slots(
-    matches: &ArgMatches,
-    config_path: &str,
-) -> anyhow::Result<()> {
+pub async fn fill_missing_slots(matches: &ArgMatches, config_path: &str) -> anyhow::Result<()> {
     let limit = matches.get_one::<i64>("limit").unwrap();
     let cfg = Config::load(config_path).await?;
     let mut conn = db::new_connection(&cfg.db_url)?;
@@ -16,10 +13,30 @@ pub async fn fill_missing_slots(
 
     let client = db::client::Client {};
 
-    let mut  blocks = client.partial_blocks(&mut conn, *limit)?;
+    let mut blocks = client.partial_blocks(&mut conn, *limit)?;
     for block in blocks.iter_mut() {
         let block_data: UiConfirmedBlock = serde_json::from_value(block.data.clone())?;
         let slot = block_data.parent_slot + 1;
+        let sample_tx = block_data
+            .transactions
+            .clone()
+            .and_then(|vec| vec.into_iter().next());
+        let sample_tx_hash = if let Some(tx) = sample_tx {
+            if let EncodedTransaction::Json(tx) = &tx.transaction {
+                tx.signatures.clone()
+            } else {
+                vec![]
+            }
+        } else {
+            vec![]
+        };
+        log::info!(
+            "block(slot={slot}, height={}, parent_slot={}, block_hash={}, sample_tx_hash={:?})",
+            block.number,
+            block_data.parent_slot,
+            block_data.blockhash,
+            sample_tx_hash
+        );
         client.update_block_slot(&mut conn, block.number, slot as i64)?;
     }
 
