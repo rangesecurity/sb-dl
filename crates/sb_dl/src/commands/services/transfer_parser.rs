635323ff77f0c0b60a7bad0baac2662363a9d57c
bonedaddy <alex@bonedaddy.io>
2024-09-19 12:31:14 -0700

    update transfer_parser command

--- crates/sb_dl/src/commands/services/transfer_parser.rs
+++ crates/sb_dl/src/commands/services/transfer_parser.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use db::{client::{BlockFilter, Client}, migrations::run_migrations, models::{BlockTableChoice, Blocks}};
 use sb_dl::{config::Config, services::{transfer_flow_api::serve_api, transfer_parser::TransferParser}};
 use tokio::signal::unix::{signal, SignalKind};
@@ -12,26 +13,16 @@ pub async fn transfer_parser(
     let start = *matches.get_one::<i64>("start").unwrap();
     let end = *matches.get_one::<i64>("end").unwrap();
     let cfg = Config::load(config_path).await?;
-    let (blocks_tx, blocks_rx) = tokio::sync::mpsc::channel::<Blocks>(1_000);
     let tx_parser = TransferParser::new(
-        blocks_rx,
-        &cfg.elasticsearch_url
+        &cfg.elasticsearch.url,
+        cfg.elasticsearch.storage_version
     ).await?;
     
     let mut conn = db::new_connection(&cfg.db_url)?;
     run_migrations(&mut conn);
 
-    let sig_quit = signal(SignalKind::quit())?;
-    let sig_int = signal(SignalKind::interrupt())?;
-    let sig_term = signal(SignalKind::terminate())?;
-    let (exit_tx, exit_rx) = tokio::sync::oneshot::channel::<()>();
-    let (_finished_tx, finished_rx) = tokio::sync::oneshot::channel();
-
-    tokio::task::spawn(async move {
-        tx_parser.start(exit_rx).await;
-    });
-
     let client = Client{};
+    log::info!("fetching blocks");
     for block in start..=end {
         match client.select_block(
             &mut conn,
@@ -39,19 +30,17 @@ pub async fn transfer_parser(
             blocks_table
         ) {
             Ok(mut blocks) => if blocks.is_empty() {
-                log::debug!("failed to find block({block})");
+                log::warn!("failed to find block({block})");
                 continue;
             } else {
-                if let Err(err) = blocks_tx.send(std::mem::take(&mut blocks[0])).await {
-                    log::error!("failed to send block({block}) {err:#?}");
-                }
+                let block_number = blocks[0].number;
+                tx_parser.start(vec![std::mem::take(&mut blocks[0])]).await.with_context(|| "indexing failed")?;
+                log::info!("indexed block({block_number})");
             }
             Err(err) => {
                 log::debug!("failed to query db {err:#?}");
             }
         }
     }
-    let _ = handle_exit(sig_quit, sig_int, sig_term, finished_rx).await;
-    let _ =  exit_tx.send(());
     Ok(())
 }
