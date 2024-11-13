4493600bdfd183922ad647c140c7a7c9605af3d4
bonedaddy <alex@bonedaddy.io>
2024-11-13 11:41:03 -0800

    use batch transactions for inserting squads data

--- crates/sb_dl/src/commands/services/squads_indexer.rs
+++ crates/sb_dl/src/commands/services/squads_indexer.rs
@@ -1,6 +1,7 @@
 use std::{str::FromStr, time::Duration};
-
+use chrono::prelude::*;
 use db::{client::Client, migrations::run_migrations, new_connection};
+use diesel::Connection;
 use sb_dl::{config::Config, programs::squads::{v3::MultisigV3, v4::MultisigV4}, services::{idl_indexer::IdlIndexer, squads_indexer::SquadsIndexer}};
 use solana_sdk::pubkey::Pubkey;
 
@@ -21,37 +22,52 @@ pub async fn index_multisigs(matches: &clap::ArgMatches, config_path: &str) -> a
         ticker.tick().await;
         let v4_msigs = indexer.fetch_multisigs_v4().await?;
         log::info!("found {} v4 multisig accounts", v4_msigs.len());
-        for (account, msig_info) in v4_msigs.into_iter() {
-            let vault = MultisigV4::derive_vault_pda(&account, 0).0;
-            if let Err(err) = client.insert_or_update_squads(
-                &mut conn,
-                &account.to_string(),
-                &[vault.to_string()],
-                &msig_info.members.into_iter().map(|member| member.key.to_string()).collect::<Vec<_>>(),
-                msig_info.threshold as i32,
-                4
-            ) {
-                log::error!("failed to record multisig(v4=true, account={account}) {err:#?}");
+        let start = Utc::now();
+        if let Err(err) = conn.transaction::<_, anyhow::Error, _>(|conn| {
+            for (account, msig_info) in v4_msigs.into_iter() {
+                let vault = MultisigV4::derive_vault_pda(&account, 0).0;
+                if let Err(err) = client.insert_or_update_squads(
+                    conn,
+                    &account.to_string(),
+                    &[vault.to_string()],
+                    &msig_info.members.into_iter().map(|member| member.key.to_string()).collect::<Vec<_>>(),
+                    msig_info.threshold as i32,
+                    4
+                ) {
+                    log::error!("failed to record multisig(v4=true, account={account}) {err:#?}");
+                }
             }
+            Ok(())
+        }) {
+            log::error!("failed to insert v4 multisigs {err:#?}");
         }
+        log::info!("took {} seconds to insert v4 records", Utc::now().signed_duration_since(start).num_seconds());
+
         let v3_msigs = indexer.fetch_multisigs_v3().await?;
         log::info!("found {} v3 multisig accounts", v3_msigs.len());
-        for (account, msig_info) in v3_msigs.into_iter() {
-            // vault index 0 is reserved for internal usage only
-            let vaults = (1..=msig_info.authority_index).into_iter().map(|idx| {
-                MultisigV3::derive_vault_pda(&account, idx as u32).0.to_string()
-            }).collect::<Vec<_>>();
-            if let Err(err) = client.insert_or_update_squads(
-                &mut conn,
-                &account.to_string(),
-                &vaults,
-                &msig_info.keys.into_iter().map(|key| key.to_string()).collect::<Vec<_>>(),
-                msig_info.threshold as i32,
-                3
-            ) {
-                log::error!("failed to record multisig(v3=true, account={account}) {err:#?}");
+        let start = Utc::now();
+        if let Err(err) = conn.transaction::<_, anyhow::Error,_>(|conn| {
+            for (account, msig_info) in v3_msigs.into_iter() {
+                // vault index 0 is reserved for internal usage only
+                let vaults = (1..=msig_info.authority_index).into_iter().map(|idx| {
+                    MultisigV3::derive_vault_pda(&account, idx as u32).0.to_string()
+                }).collect::<Vec<_>>();
+                if let Err(err) = client.insert_or_update_squads(
+                    conn,
+                    &account.to_string(),
+                    &vaults,
+                    &msig_info.keys.into_iter().map(|key| key.to_string()).collect::<Vec<_>>(),
+                    msig_info.threshold as i32,
+                    3
+                ) {
+                    log::error!("failed to record multisig(v3=true, account={account}) {err:#?}");
+                }
             }
+            Ok(())
+        }) {
+            log::error!("failed to insert v3 multisigs {err:#?}");
         }
+        log::info!("took {} seconds to insert v3 records", Utc::now().signed_duration_since(start).num_seconds());
     }
     Ok(())
 }
